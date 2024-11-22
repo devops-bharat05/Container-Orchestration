6c8f08d5eb05fef802a3db8ce08b4d72cad70f84
Bharat Bhushan <116905352+devops-bharat05@users.noreply.github.com>
2024-11-22 12:07:01 +0530

    Create install_tools.sh

---
+++ install_tools.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+ehco " Updating...."
+sudo apt-get update
+
+#AWS CLI Installation
+echo "Installing AWS CLI......."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf awscliv2.zip aws/
+
+#Helm Installation
+echo "Installing Helm......."
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm -f get_helm.sh
+
+#eksctl Installation
+echo "Installing eksctl......."
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+curl -sL "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_checksums.txt" | grep $PLATFORM | sha256sum --check
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+
+# kubectl Installation
+echo "Installing kubectl......."
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+rm -f kubectl kubectl.sha256
+
+# Validate Installations
+echo "Validating installations......."
+echo "AWS CLI Version:"
+aws --version
+
+echo "Helm Version:"
+helm version --short
+
+echo "eksctl Version:"
+eksctl version
+
+echo "kubectl Version:"
+kubectl version --client --short
+
+echo "Installation of all tools completed successfully!"
